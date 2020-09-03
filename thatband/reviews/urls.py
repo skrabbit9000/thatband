c1e8faf725dd52ff0183310ac7a158b06cb87317
Mike Hansen <mike@endovalley.com>
2020-09-02 19:55:57 -0600

    added update & delete

--- thatband/reviews/urls.py
+++ thatband/reviews/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from .views import BandListView, BandDetailView, BandAddView
+from .views import (
+    BandListView,
+    BandDetailView,
+    BandAddView,
+    BandUpdateView,
+    BandDeleteView,
+)
 
 urlpatterns = [
     path("", BandListView.as_view(), name="home"),
-    path('band/<int:pk>/', BandDetailView.as_view(), name='band'),
-    path('band/add', BandAddView.as_view(), name='add_band'),
+    path("band/<int:pk>/", BandDetailView.as_view(), name="band"),
+    path("band/add/", BandAddView.as_view(), name="add_band"),
+    path("band/<int:pk>/update/", BandUpdateView.as_view(), name="update_band"),
+    path("band/<int:pk>/delete/", BandDeleteView.as_view(), name="delete_band"),
 ]
--- thatband/reviews/views.py
+++ thatband/reviews/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 from .models import Review
 
 
@@ -13,7 +15,19 @@ class BandDetailView(DetailView):
     model = Review
     template_name = "band.html"
 
+
 class BandAddView(CreateView):
     model = Review
     template_name = "add_band.html"
-    fields = ['band_name', 'rating', 'genre', 'description', 'sounds_like'] 
+    fields = ["band_name", "rating", "genre", "description", "sounds_like"]
+
+
+class BandUpdateView(UpdateView):
+    model = Review
+    template_name = "update_band.html"
+    fields = ["band_name", "rating", "genre", "description", "sounds_like"]
+
+class BandDeleteView(DeleteView):
+    model = Review
+    template_name = "delete_band.html"
+    success_url = reverse_lazy('home')
