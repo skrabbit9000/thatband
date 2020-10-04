e83c6ee2ddaa4fe51973a62f365fade7be0a96f4
Mike Hansen <mike@endovalley.com>
2020-10-04 12:39:48 -0600

    bootstrap updates and crispy forms

--- thatband/thatband/settings.py
+++ thatband/thatband/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Crispy forms
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
